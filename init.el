619a64494de2ecd5c07637b275cee1601b0ee7ee
Alex Yan <41834091+yan42685@users.noreply.github.com>
2018-10-03 12:36:34 +0800

    Update init.el

--- init.el
+++ init.el
@@ -1,10 +1,12 @@
 ;; -*- mode: emacs-lisp -*-
 ;; This file is loaded by Spacemacs at startup.
 ;; It must be stored in your home directory.
-
+;;########################################################
+;;
 ;; ALEX注：
 ;;1.把typescript的两个package注释掉了，产生了两个error
-
+;;
+;;########################################################
 (defun dotspacemacs/layers ()
   "Configuration Layers declaration.
 You should not put any user code in this function besides modifying the variable
